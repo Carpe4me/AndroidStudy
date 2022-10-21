ad2c27f30e3e2f01e7c34efffdd054ba0d254ffc
AllIsWell <carpe4me@gmail.com>
2022-10-22 01:38:48 +0900

    기본 프로젝트 테스트 파일 추가

--- app/src/main/java/com/study/android/MainActivity.kt
+++ app/src/main/java/com/study/android/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.study.android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.study.android.databinding.ActivityMainBinding
+import com.study.android.morden.java.RoomActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.roomJava.setOnClickListener {
+            startActivity(
+                Intent(
+                    this,
+                    RoomActivity::class.java
+                )
+            )
+        }
     }
 }
