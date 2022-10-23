3233621a7b3935811396986609e62a89f4ff1638
AllIsWell <carpe4me@gmail.com>
2022-10-24 00:39:45 +0900

    DataBinding 사용: Viewbinding 제거

--- app/src/main/java/com/study/android/morden/java/RoomActivity.java
+++ app/src/main/java/com/study/android/morden/java/RoomActivity.java
@@ -3,31 +3,25 @@ package com.study.android.morden.java;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.study.android.R;
 import com.study.android.databinding.ActivityRoomBinding;
 
 public class RoomActivity extends AppCompatActivity {
 
-    private ActivityRoomBinding binding;
+    private ActivityRoomBinding viewBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityRoomBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        //viewBinding = ActivityRoomBinding.inflate(getLayoutInflater());
+        ActivityRoomBinding dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_room);
+        dataBinding.setLifecycleOwner(this);
 
         RoomViewModel viewModel = new ViewModelProvider(this).get(RoomViewModel.class);
-        // UI 갱신
-        viewModel.getAll().observe(this, todos -> {
-            binding.result.setText(todos.toString());
-        });
-
-        // 버튼 클릭시 DB에 Insert
-        binding.addButton.setOnClickListener(v -> {
-            viewModel.insert(new Todo(binding.todoEdit.getText().toString()));
-        });
-
+        dataBinding.setViewModel(viewModel);
     }
 
 }
--- app/src/main/java/com/study/android/morden/java/RoomViewModel.java
+++ app/src/main/java/com/study/android/morden/java/RoomViewModel.java
@@ -12,22 +12,24 @@ import java.util.List;
 
 public class RoomViewModel extends AndroidViewModel {
 
+    public LiveData<List<Todo>> todos;
+    public String newTodo;
     AppDatabase db;
 
     public RoomViewModel(@NonNull Application application) {
         super(application);
         db = Room.databaseBuilder(application, AppDatabase.class, "todo-db_java")
                 .build();
+        todos = db.todoDao().getAll();
     }
 
     public LiveData<List<Todo>> getAll() {
         return db.todoDao().getAll();
     }
 
-    public void insert(Todo todo){
-        new InsertAyncTask(db.todoDao()).execute(todo);
+    public void insert(String todo){
+        new InsertAyncTask(db.todoDao()).execute(new Todo(todo));
     }
-
 
     private static class InsertAyncTask extends AsyncTask<Todo, Void, Void> {
         private TodoDao mTodoDao;
